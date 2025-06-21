1264e9d39fb293510a16f9036db24ac916b08799
yashKumar <yash941200kumar@gmail.com>
2025-06-21 10:03:05 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(value) {
+  document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function deleteOne() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    document.getElementById("display").value = eval(
+      document.getElementById("display").value
+    );
+  } catch {
+    document.getElementById("display").value = "Error";
+  }
+}
